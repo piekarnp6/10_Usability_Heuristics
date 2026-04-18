a42ed090dbe3d95b74c36db43bfb9391e171514a
Piotr Piekarniak <P.Piekarniak@wit.edu.pl>
2026-04-18 11:58:49 +0200

    Refactor navigation menu toggle functionality

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const nav = document.getElementsByTagName('nav')[0];
 const menuToggle = document.getElementById('menuToggle');
 
-// Toggle Menu (Open/Close with one button)
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
 
-// Close menu when a link is clicked
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
